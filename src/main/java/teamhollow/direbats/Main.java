5c90bafb1140db74493c62c484ddbac402361028
Daniel Orr <orrdaniel73@gmail.com>
2020-10-03 15:15:58 +0100

    Update to 1.16.3 (Forge) (#3)
    * 1.16.3
    
    * Updated build.grade
    
    Co-authored-by: Jon Lachney <jonlachney@gmail.com>

--- src/main/java/teamhollow/direbats/Main.java
+++ src/main/java/teamhollow/direbats/Main.java
@@ -5,10 +5,11 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.MobSpawnInfo;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -23,46 +24,47 @@ import teamhollow.direbats.init.ModEntities;
 
 @Mod(Main.MODID)
 public class Main {
-    public static final String MODID = "direbats";
-    public static final ItemGroup DIREBAT = new ItemGroup("direbats.direbatstab") {
-        @OnlyIn(Dist.CLIENT)
-        public ItemStack createIcon() {
-            return new ItemStack(Items.TORCH);
-        }
-    };
+	public static final String MODID = "direbats";
+	public static final ItemGroup DIREBAT = new ItemGroup("direbats.direbatstab") {
+		@OnlyIn(Dist.CLIENT)
+		public ItemStack createIcon() {
+			return new ItemStack(Items.TORCH);
+		}
+	};
 
-    public Main() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the clientSetup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+	public Main() {
+		// Register the setup method for modloading
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		// Register the enqueueIMC method for modloading
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+		// Register the processIMC method for modloading
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+		// Register the clientSetup method for modloading
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+		// Register spawns
+		MinecraftForge.EVENT_BUS.addListener(Main::onBiomesLoaded);
+		// Register ourselves for server and other game events we are interested in
+		MinecraftForge.EVENT_BUS.register(this);
+	}
 
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-    }
+	private void setup(final FMLCommonSetupEvent event) {
 
-    private void setup(final FMLCommonSetupEvent event) {
-        for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
-            if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.OVERWORLD)) {
-                biome.getSpawns(EntityClassification.AMBIENT).add(new Biome.SpawnListEntry(ModEntities.DIREBAT, 5, 1, 2));
-            }
-        }
-    }
+	}
 
-    private void clientSetup(final FMLClientSetupEvent event) {
-        RenderingRegistry.registerEntityRenderingHandler(ModEntities.DIREBAT, DirebatEntityRenderer::new);
-        RenderingRegistry.registerEntityRenderingHandler(ModEntities.FANGARROW, FangArrowEntityRenderer::new);
-    }
+	public static void onBiomesLoaded(final BiomeLoadingEvent event) {
+			if (event.getCategory() != Biome.Category.NETHER && event.getCategory() != Biome.Category.THEEND && event.getCategory() != Biome.Category.NONE) {
+				event.getSpawns().withSpawner(EntityClassification.AMBIENT, new MobSpawnInfo.Spawners(ModEntities.DIREBAT, 5, 1, 2));
+			}
+	}
 
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-    }
+	private void clientSetup(final FMLClientSetupEvent event) {
+		RenderingRegistry.registerEntityRenderingHandler(ModEntities.DIREBAT, DirebatEntityRenderer::new);
+		RenderingRegistry.registerEntityRenderingHandler(ModEntities.FANGARROW, FangArrowEntityRenderer::new);
+	}
 
-    private void processIMC(final InterModProcessEvent event)
-    {
-    }
+	private void enqueueIMC(final InterModEnqueueEvent event) {
+	}
+
+	private void processIMC(final InterModProcessEvent event) {
+	}
 }
